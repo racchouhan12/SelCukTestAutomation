066700c144bdf6df0cfb7e6cc880634b819b6334
RakeshSingh Chouhan <rakeshsingh.chouhan@ness.com>
2018-08-10 18:39:33 +0530

    Added logger

--- src/test/java/com/test/automation/screens/BaseScreen.java
+++ src/test/java/com/test/automation/screens/BaseScreen.java
@@ -1,14 +1,18 @@
 package com.test.automation.screens;
 
 import com.test.automation.utilities.ThisRun;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public abstract class BaseScreen {
 
     final WebDriver driver;
+    protected static Logger logger = LogManager.getLogger(BaseScreen.class.getName());
 
     protected BaseScreen() {
+        logger.debug("BaseScreen intialized...");
         ThisRun thisRun = ThisRun.getInstance();
         driver = thisRun.driver();
 
